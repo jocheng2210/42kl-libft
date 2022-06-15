c816d03c379ee02789cfaccf6c96ad9fd3f3ee7c
Joshua Cheng <jocheng@u80z04s06.42kl.edu.my>
2022-06-15 22:09:23 +0800

    i did split

--- ft_split.c
+++ ft_split.c
@@ -6,56 +6,142 @@
 /*   By: jocheng <jocheng@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 18:43:35 by jocheng           #+#    #+#             */
-/*   Updated: 2022/06/09 22:48:17 by jocheng          ###   ########.fr       */
+/*   Updated: 2022/06/15 22:08:02 by jocheng          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	malloc_count(char const *s, char c)
+int	count_words(char const *s, char c)
 {
-	char	*checker;
-	int		string_count;
+	int	words;
+	int	s_count;
 
-	string_count = 0;
-	checker = ft_strdup(s);
-	while (*checker != '\0')
-	{
-		checker = ft_strchr((checker), c);
-		string_count++;
-		checker++;
-	}
-	return (string_count);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**ret;
-	int		ret_i;
-	int		ret_j;
-	int		s_count;
-
-	ret_i = 0;
-	ret_j = 0;
+	words = 0;
 	s_count = 0;
-	ret = malloc(sizeof(char) * (ft_strlen(s) + malloc_count(s, c)));
-	if (!s || !c || !ret)
-		return (NULL);
-	if (s[0] == c)
+	while (s[s_count] == c && s[s_count] != '\0')
 	{
-		ret[ret_i][ret_j] = s[s_count];
-		ret_j++;
+		s_count++;
 	}
 	while (s[s_count] != '\0')
 	{
-		ret_j = 0;
-		if (s[s_count] == c)
+		words++;
+		while (s[s_count] != c && s[s_count] != '\0')
 		{
-			ret_i++;
+			s_count++;
 		}
-		ret[ret_i][ret_j] = s[s_count];
-		ret_j++;
+		while (s[s_count] == c && s[s_count] != '\0')
+		{
+			s_count++;
+		}
+	}
+	words++;
+	return (words);
+}
+
+int	count_char(char const *s, char c)
+{
+	int	letter;
+	int	s_count;
+
+	letter = 0;
+	s_count = 0;
+	while (s[s_count] != c && s[s_count] != '\0')
+	{
+		letter++;
 		s_count++;
 	}
+	letter++;
+	return (letter);
+}
+
+char	**ft_split(char	const *s, char c)
+{
+	int		i;
+	int		j;
+	char	**ret;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	ret = malloc(sizeof(char *) * count_words(s, c));
+	if (!ret || !s)
+		return (NULL);
+	while (*s == c && *s != '\0')
+		*s++;
+	while (*s != '\0')
+	{
+		j = 0;
+		if (*s != c)
+			ret[i] = malloc(sizeof(char) * count_char(s, c));
+		while (*s != c && *s != '\0')
+			ret[i][j++] = *s++;
+		ret[i++][j] = '\0';
+		while (*s == c && *s != '\0')
+			s++;
+	}
+	ret[i] = 0;
 	return (ret);
 }
+
+/*
+char	**ft_split(char	const *s, char c)
+{
+	int		i;
+	int		j;
+	int     l;
+	char	**ret;
+
+	i = 0;
+	j = 0;
+	if (!s || !c)
+		return (NULL);
+	ret = malloc(sizeof(char *) * count_words(s, c));
+	if (!ret)
+		return (NULL);
+	while (*s == c)
+	{
+		*s++;
+	}
+	while (*s != '\0')
+	{
+		if (*s != c)
+		{
+			ret[i] = malloc(sizeof(char) * count_char(s, c));
+			l = count_char(s, c) - 1;
+		}
+		while (l != 0)
+		{
+			ret[i][j] = *s;
+			s++;
+			j++;
+			l--;
+		}
+		ret[i][j] = '\0';
+		i++;
+		j = 0;
+		while (*s == c)
+		{
+			s++;
+		}
+	}
+	ret[i] = 0;
+	return (ret);
+}
+*/
+/*
+int main()
+{
+	char	str[] = "aaacaababcabcdabcdeabcdeabcdef";
+	char	**ret;
+	char	c = 'a';
+	int		i;
+	
+    i = 0;
+    ret = ft_split(str, c);
+    while (ret[i] != '\0')
+    {
+        printf("%s\n", ret[i]);
+        i++;
+    }
+}*/
